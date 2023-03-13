e11913b31e409bcc58bd7c7d19f91b9498257993
Abd0 <abdoali345757@gmail.com>
2023-03-13 14:51:31 +0200

    Abdulrahman Khalifa "DataBase Connection in Emp Form"

--- Employees.cs
+++ Employees.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,7 +17,8 @@ namespace Dairy_Farm_Management_System
         {
             InitializeComponent();
         }
-
+        SqlConnection con = new SqlConnection(@"Data Source=
+        (LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\defds\Documents\DailyFarmDB.mdf;Integrated Security=True;Connect Timeout=30");
         private void SaveBtn_Click(object sender, EventArgs e)
         {
 
