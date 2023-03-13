a47b5ff1a26f7e6bc63c6dccbd2b117cd46fd8a5
Abd0 <abdoali345757@gmail.com>
2023-03-13 15:00:10 +0200

    Abdulrahman Khalifa "show Emp Form"

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Dairy_Farm_Management_System
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Breedings());
+            Application.Run(new Employees());
         }
     }
 }
