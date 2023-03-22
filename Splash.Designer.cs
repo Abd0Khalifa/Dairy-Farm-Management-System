2503669cb3589442424b6ef78085b47c322d7ed8
Abd0 <abdoali345757@gmail.com>
2023-03-22 03:22:24 +0200

    Abdulrahman Khalifa "Cal timer Func in Splash Form"

--- Splash.Designer.cs
+++ Splash.Designer.cs
@@ -88,6 +88,7 @@ namespace Dairy_Farm_Management_System
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
--- Splash.cs
+++ Splash.cs
@@ -31,5 +31,10 @@ namespace Dairy_Farm_Management_System
                 Log.Show();
             }
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
     }
 }
