bd4922979733a7c9e55417db33a9b938d780aba8
Abd0 <abdoali345757@gmail.com>
2023-03-22 03:45:05 +0200

    Abdulrahman Khalifa "Start in Coding LoginBtn Login Form"

--- Login.cs
+++ Login.cs
@@ -19,7 +19,21 @@ namespace Dairy_Farm_Management_System
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            if (RoleCb.SelectedIndex == -1)
+            {
+                MessageBox.Show("Select Role");
+            }
+            if (RoleCb.SelectedItem.ToString() == "Admin")
+            {
+                Employees emp = new Employees();
+                emp.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Wrong Admin Name or Password");
+            }
+            
         }
 
         private void label3_Click(object sender, EventArgs e)
